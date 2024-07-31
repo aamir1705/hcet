8218041c6501dffeb0b68bbab037b42397033e65
aamir <aamir.1705@gmail.com>
2024-07-31 21:30:12 +0530

    just a change

--- pages/contact.vue
+++ pages/contact.vue
@@ -8,9 +8,7 @@
           <div class="col-md-12">
             <div class="row probootstrap-gutter0">
               <div class="col-md-4" id="probootstrap-sidebar">
-                <div
-                  class="probootstrap-sidebar-inner probootstrap-overlap probootstrap-animate"
-                >
+                <div class="probootstrap-sidebar-inner probootstrap-overlap probootstrap-animate">
                   <h3>Pages</h3>
                   <ul class="probootstrap-side-menu">
                     <li><a href="/">Home</a></li>
@@ -25,43 +23,30 @@
               <div class="flex flex-col gap-10">
                 <h2>Get In Touch</h2>
                 <p>Contact us using the form below.</p>
-                <form  action="#" method="post" class="flex flex-col gap-10">
+                <form action="#" method="post" class="flex flex-col gap-10">
                   <div>
                     <label for="name">Full Name</label>
-                    <input
-                      type="text"
+                    <input type="text"
                       class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                      id="name"
-                      name="name"
-                    />
+                      id="name" name="name" />
                   </div>
                   <div>
                     <label for="email">Email</label>
-                    <input
-                      type="email"
+                    <input type="email"
                       class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                      id="email"
-                      name="email"
-                    />
+                      id="email" name="email" />
                   </div>
                   <div>
                     <label for="subject">Subject</label>
-                    <input
-                      type="text"
+                    <input type="text"
                       class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                      id="subject"
-                      name="subject"
-                    />
+                      id="subject" name="subject" />
                   </div>
                   <div>
                     <label for="message">Message</label>
-                    <textarea
-                      cols="30"
-                      rows="10"
+                    <textarea cols="30" rows="10"
                       class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                      id="message"
-                      name="message"
-                    ></textarea>
+                      id="message" name="message"></textarea>
                   </div>
                   <div>
                     <button type="submit" class="bg-[#49D292] border-[#49D292] py-4 px-12 text-white rounded-full ">
@@ -86,12 +71,8 @@
                   <div class="item">
                     <iframe
                       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3668.1881730856235!2d79.99478101535573!3d23.16333126686425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7a8002ac2a17d69!2sHitkarini%20College%20of%20Engineering%20and%20Technology!5e0!3m2!1sen!2sin!4v1614106959661!5m2!1sen!2sin"
-                      width="1000"
-                      height="500"
-                      allowfullscreen=""
-                      loading="lazy"
-                      style="overflow: hidden; position: inherit; border: 0"
-                    ></iframe>
+                      width="1000" height="500" allowfullscreen="" loading="lazy"
+                      style="overflow: hidden; position: inherit; border: 0"></iframe>
                   </div>
                 </li>
               </ul>
@@ -100,5 +81,6 @@
         </div>
       </div>
     </section>
+    <div>check github change </div>
   </AppContainer>
 </template>
